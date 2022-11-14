4ec9437dc604ffbcf045712cbd25b0a8884aee56
rweil55 <github@shaw-weil.com>
2022-11-14 08:03:05 -0500

    2022-11-14

--- DisplayTrails.php
+++ DisplayTrails.php
@@ -1,25 +1,73 @@
-<?php
-
-class DisplayTrails {
-    public static function Display( $attr ) {
-        global $eol, $errorBeg, $errorEnd;
-        global $rrw_trails;
-        $msg = "";
-
-        $action = rrwUtil::fetchparameterString( "action" );
-        tablename( "$rrw_trails" );
-        sortdefault( "trailName" );
-        seqname( "trailName" );
-        columns( "Trail Name", "trailName", 69 );
-        columns( "Trail Short Name", "trail_short_name", 69 );
-        DropDownSelf( "Corridor", "corridor", 69 );
-         
-        if ( empty( $action ) ) {
-            $msg .= listdata();
-            return $msg;
-        }
-        $msg .= DoAction();
-        return $msg;
-    }   // end function
-} // end class
-?>
+<?php
+
+
+
+
+
+class DisplayTrails {
+
+
+    public static function Display( $attr ) {
+
+
+        global $eol, $errorBeg, $errorEnd;
+
+
+        global $rrw_trails;
+
+
+        $msg = "";
+
+
+
+
+
+        $action = rrwUtil::fetchparameterString( "action" );
+
+
+        tablename( "$rrw_trails" );
+
+
+        sortdefault( "trailName" );
+
+
+        seqname( "trailName" );
+
+
+        columns( "Trail Name", "trailName", 69 );
+
+
+        columns( "Trail Short Name", "trail_short_name", 69 );
+
+
+        DropDownSelf( "Corridor", "corridor", 69 );
+
+
+         
+
+
+        if ( empty( $action ) ) {
+
+
+            $msg .= listdata();
+
+
+            return $msg;
+
+
+        }
+
+
+        $msg .= DoAction();
+
+
+        return $msg;
+
+
+    }   // end function
+
+
+} // end class
+
+
+?>
