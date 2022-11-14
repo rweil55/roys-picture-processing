a089bf975b46389cf3dfdb9dfac0679d697a5605
rweil55 <github@shaw-weil.com>
2022-11-14 07:40:08 -0500

    2022-11-14

--- roys-picture-processng.php
+++ roys-picture-processng.php
@@ -11,7 +11,7 @@
  * Text Domain: Roys-picture-processng
  * Domain Path: /translation
  
-  * Version: 2.1.73
+  * Version: 2.1.75
  */
 // disable direct access
 ini_set( "display_errors", true );
