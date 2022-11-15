c83ec43382e008b80beac44f04c957f47fd2856c
rweil55 <github@shaw-weil.com>
2022-11-15 10:28:07 -0500

    2022-11-15

--- DisplayPhotogaphers.php
+++ DisplayPhotogaphers.php
@@ -1,29 +1,29 @@
-<?php
-
-require_once "display_tables_class.php";
-
-class DisplayPhotographers {
-    public static function Display( $attr ) {
-        global $eol, $errorBeg, $errorEnd;
-        global $rrw_photographers;
-        $msg = "";
-        
-        $TABLE  = new rrwDisplayTable();
-        $action = rrwUtil::fetchparameterString( "action" );
-        $TABLE->tablename( "$rrw_photographers" );
-        $TABLE->sortdefault( "photographer" );
-        $TABLE->seqname( "photographer" );
-        $TABLE->columns( "photographer Name", "photographer", 69 );
-        $TABLE->columns( "E-Mail", "Address", 69 );
-        $TABLE->columns( "Comment", "Comment", 69 );
-        $TABLE->columns( "copyright Default", "copyrightDefault", 69 );
-        
-        if ( empty( $action ) ) {
-            $msg .= $TABLE->listdata();
-            return $msg;
-        }
-        $msg .= $TABLE->DoAction();
-        return $msg;
-    }   // end function
-} // end class
+<?php
+
+require_once "display_tables_class.php";
+
+class DisplayPhotographers {
+    public static function Display( $attr ) {
+        global $eol, $errorBeg, $errorEnd;
+        global $rrw_photographers;
+        $msg = "";
+        
+        $TABLE  = new rrwDisplayTable();
+        $action = rrwUtil::fetchparameterString( "action" );
+        $TABLE->tablename( "$rrw_photographers" );
+        $TABLE->sortdefault( "photographer" );
+        $TABLE->seqname( "photographer" );
+        $TABLE->columns( "photographer Name", "photographer", 69 );
+        $TABLE->columns( "E-Mail", "Address", 69 );
+        $TABLE->columns( "Comment", "Comment", 69 );
+        $TABLE->columns( "copyright Default", "copyrightDefault", 69 );
+        
+        if ( empty( $action ) ) {
+            $msg .= $TABLE->listdata();
+            return $msg;
+        }
+        $msg .= $TABLE->DoAction();
+        return $msg;
+    }   // end function
+} // end class
 ?>
