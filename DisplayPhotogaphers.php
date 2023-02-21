4dc44547871134eb05b9533d54ac9cb95f337932
rweil55 <github@shaw-weil.com>
2023-02-21 16:01:35 -0500

    2023-02-21

--- DisplayPhotogaphers.php
+++ DisplayPhotogaphers.php
@@ -2,24 +2,26 @@
 
 require_once "display_tables_class.php";
 
-class DisplayPhotographers {
-    public static function Display( $attr ) {
+class DisplayPhotographers
+{
+    public static function Display($attr)
+    {
         global $eol, $errorBeg, $errorEnd;
         global $rrw_photographers;
         $msg = "";
-        
+
         $TABLE  = new rrwDisplayTable();
-        $action = rrwUtil::fetchparameterString( "action" );
-        $TABLE->tablename( "$rrw_photographers" );
-        $TABLE->sortdefault( "photographer" );
-        $TABLE->keyname( "photographer" );
-        $TABLE->columns( "photographer Name", "photographer", 69 );
-        $TABLE->columns( "E-Mail", "Address", 69 );
-        $TABLE->columns( "Comment", "Comment", 69 );
-        $TABLE->columns( "copyright Default", "copyrightDefault", 69 );
-        
-        if ( empty( $action ) ) {
-            $msg .= $TABLE->listdata();
+        $action = rrwUtil::fetchparameterString("action");
+        $TABLE->tablename("$rrw_photographers");
+        $TABLE->sortdefault("photographer");
+        $TABLE->keyname("photographer");
+        $TABLE->columns("photographer Name", "photographer", 69);
+        $TABLE->columns("E-Mail", "Address", 69);
+        $TABLE->columns("Comment", "Comment", 69);
+        $TABLE->columns("copyright Default", "copyrightDefault", 69);
+
+        if (empty($action)) {
+            $msg .= $TABLE->DoAction("list");
             return $msg;
         }
         $msg .= $TABLE->DoAction();
